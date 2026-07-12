6d3c29403ee65634497db091be90fffff3c4bfef
Mihlali <sivuyilesifuba@gmail.com>
2026-07-12 13:18:30 +0200

    Add RMSprop optimizer with tests

--- tensors/optim/__init__.py
+++ tensors/optim/__init__.py
@@ -3,5 +3,6 @@
 from .optimizer import Optimizer
 from .sgd import SGD
 from .adam import Adam
+from .rmsprop import RMSprop
 
-__all__ = ["Optimizer", "SGD", "Adam"]
+__all__ = ["Optimizer", "SGD", "Adam", "RMSprop"]
